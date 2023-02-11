c489d2ebab22e0eb64cd8c08946d3df1d4a623f2
nsunwoo <sunwoo.stats@gmail.com>
2023-02-11 23:44:06 +0900

    feat: Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as config from 'config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const conf = new DocumentBuilder()
+    .setTitle('board-app')
+    .setDescription('made with NestJS')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, conf);
+  SwaggerModule.setup('api', app, document);
 
   const serverConfig = config.get('server');
   const port = serverConfig.port;
